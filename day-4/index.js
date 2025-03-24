33d48f1aea5ab072c75ef2a9fd2a01c812650504
Abhijitam01 <abhijitamdubey31@gmail.com>
2025-03-24 15:21:59 +0000

    day 4 classes

---
+++ day-4/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+
+const app = express()
+
+app.get("/sum", function(req,res){
+
+})
+app.get("/sub", function(req,res){
+    
+})
+app.get("/mul", function(req,res){
+    
+})
+app.get("/div", function(req,res){
+    
+})
